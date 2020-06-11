0fd216218e3b1daaf40a5c739cf702f38ba828d7
Manny Dheer <mannydheer@gmail.com>
2020-06-11 17:55:08 -0400

    feat: request animation frame done

--- js/main.js
+++ js/main.js
@@ -3,15 +3,44 @@ import { Canvas } from "./Canvas.js";
 //constants.
 import { GAME_WIDTH, GAME_HEIGHT } from "./GameConstants.js";
 
-console.log("bitch");
 //instantiate the class Engine once.
 const gameEngine = new Engine(document.getElementById("app"));
-//pass the game root to the Canvas.
-const newCanvas = new Canvas(
-  "gameCanvas",
-  document.getElementById("app"),
-  GAME_WIDTH,
-  GAME_HEIGHT
-);
 
-newCanvas.create();
+//-------------REQUEST ANIMATION FRAME ---------------
+let requestAnimationFrame = window.requestAnimationFrame;
+window.mozRequestAnimationFrame;
+window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
+let deltaTime = 0;
+let oldTimeStamp = 0;
+let fps;
+
+//-------------------GAME LOOP --------------------------
+function gameLoop(timeStamp) {
+  // Calculate the number of seconds passed since the last frame
+  //we divide by 1000 to get the delta time in seconds between each frame.
+  deltaTime = (timeStamp - oldTimeStamp) / 1000;
+  //this will have the previous time stamp for the next frame.
+  oldTimeStamp = timeStamp;
+  //calculates the frames per second.
+  fps = Math.round(1 / deltaTime);
+
+  //call the game loop for each frame.
+  requestAnimationFrame(gameLoop);
+}
+//----------------START GAME-------------------
+function startGame() {
+  requestAnimationFrame(gameLoop);
+}
+
+//Start out game.
+startGame();
+
+//pass the game root to the Canvas.
+// const newCanvas = new Canvas(
+//   "gameCanvas",
+//   document.getElementById("app"),
+//   GAME_WIDTH,
+//   GAME_HEIGHT
+// );
+
+// newCanvas.create();
