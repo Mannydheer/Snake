dd780f3997cac692d0a842d0cc4d32fdffb2c555
Manny Dheer <mannydheer@gmail.com>
2020-06-12 09:27:42 -0400

    feat: snake movement with directions done

--- js/Snake.js
+++ js/Snake.js
@@ -1,94 +1,86 @@
 export class Snake {
-  constructor(ctx) {
-    this.ctx = ctx;
-    this.color = "green";
-    this.width = 20;
-    this.height = 20;
-    this.currentDirection = null;
-    this.lastHeadPosition = {
-      x: null,
-      y: null,
-    };
-    this.snakeList = [
-      { x: 240, y: 200 },
-      { x: 220, y: 200 },
-      { x: 200, y: 200 },
-    ];
-  }
+    constructor(ctx) {
+        this.ctx = ctx;
+        this.color = "green";
+        this.width = 15;
+        this.height = 15;
+        this.currentDirection = null;
+        this.velocity = 15;
+        //this will hold the snakes pieces. 
+        //index 0 = head, 1 = body , 2 = tail.
+        this.snakeList = [{
+                x: 245,
+                y: 200
+            },
+            {
+                x: 230,
+                y: 200
+            },
+            {
+                x: 215,
+                y: 200
+            },
+        ];
 
-  drawSnake(x, y, color) {
-    this.ctx.fillStyle = color;
-    this.ctx.fillRect(x, y, this.width, this.height);
-  }
-
-  updateSnakeMovement() {
-    switch (this.currentDirection) {
-      case "up":
-        this.snakeList.forEach((snakeList, index) => {
-          if (index === 0) {
-            this.snakeList[0] = {
-              ...this.snakeList[0],
-              y: (snakeList.y -= 1),
-            };
-          }
-
-          if (
-            snakeList.y >= this.lastHeadPosition.y &&
-            snakeList.x >= this.lastHeadPosition.x
-          ) {
-            snakeList.y -= 1;
-          } else {
-            console.log("else");
-            snakeList.x += 1;
-          }
-        });
-
-        break;
-      case "down":
-        this.snakeList.forEach((snakeList, index) => {
-          if (index === 0) {
-            this.snakeList[0] = {
-              ...this.snakeList[0],
-              y: (snakeList.y += 1),
-            };
-          }
-          snakeList.y += 1;
-        });
-        break;
-      case "left":
-        this.snakeList.forEach((snakeList, index) => {
-          if (index === 0) {
-            this.snakeList[0] = {
-              ...this.snakeList[0],
-              x: (snakeList.x -= 1),
-            };
-          }
-          snakeList.x -= 1;
-        });
-
-        break;
-      case "right":
-        this.snakeList.forEach((snakeList, index) => {
-          if (index === 0) {
-            this.snakeList[0] = {
-              ...this.snakeList[0],
-              x: (snakeList.x += 1),
-            };
-          }
-          snakeList.x += 1;
-        });
-
-        break;
-
-      default:
-        break;
     }
-  }
-  updateCurrentDirection(direction) {
-    //update current direction of snake
-    this.currentDirection = direction;
-    //keep track of the last position of head.
-    //this way all the snake bodies will follow.
-    this.lastHeadPosition = this.snakeList[0];
-  }
+
+    drawSnake(x, y, color) {
+        this.ctx.fillStyle = color;
+        this.ctx.fillRect(x, y, this.width, this.height);
+    }
+
+    updateSnakeMovement() {
+        //with a for loop, we are starting the the last index of the array.
+        //as we loop, in the following order.
+        //the 2nd index (x,y) take the 1st index (x,y) positions.
+        //the 1st index  (x,y) take the 0th index (x,y) positions.
+        //the 0th index (x,y) take the NEW values of the (x,y).
+
+        for (let index = this.snakeList.length - 1; index >= 0; index--) {
+            //keep track of the snakes current direction based on keypress.
+            //Rearranging snake positions.
+
+            switch (this.currentDirection) {
+                case "up":
+                    if (index === 0) {
+                        this.snakeList[index].y = this.snakeList[index].y - this.velocity;
+                    } else {
+                        this.snakeList[index].x = this.snakeList[index - 1].x
+                        this.snakeList[index].y = this.snakeList[index - 1].y
+                    }
+                    break;
+                case "down":
+                    if (index === 0) {
+                        this.snakeList[index].y = this.snakeList[index].y + this.velocity;
+                    } else {
+                        this.snakeList[index].x = this.snakeList[index - 1].x
+                        this.snakeList[index].y = this.snakeList[index - 1].y
+                    }
+                    break;
+                case "left":
+                    if (index === 0) {
+                        this.snakeList[index].x = this.snakeList[index].x - this.velocity;
+                    } else {
+                        this.snakeList[index].x = this.snakeList[index - 1].x
+                        this.snakeList[index].y = this.snakeList[index - 1].y
+                    }
+                    break;
+                case "right":
+
+                    if (index === 0) {
+                        this.snakeList[index].x = this.snakeList[index].x + this.velocity;
+                    } else {
+                        this.snakeList[index].x = this.snakeList[index - 1].x
+                        this.snakeList[index].y = this.snakeList[index - 1].y
+                    }
+                    break;
+                default:
+                    break;
+            }
+
+        }
+
+
+    }
+
 }
--- js/main.js
+++ js/main.js
@@ -1,18 +1,27 @@
-import { Engine } from "./Engine.js";
-import { Canvas } from "./Canvas.js";
-import { Snake } from "./Snake.js";
+import {
+    Engine
+} from "./Engine.js";
+import {
+    Canvas
+} from "./Canvas.js";
+import {
+    Snake
+} from "./Snake.js";
 //constants.
-import { GAME_WIDTH, GAME_HEIGHT } from "./GameConstants.js";
+import {
+    GAME_WIDTH,
+    GAME_HEIGHT
+} from "./GameConstants.js";
 
 //instantiate the class Engine once.
 const gameEngine = new Engine(document.getElementById("app"));
 //-----------------CANVAS CLASS----------------------
 //!TODO refactor canvas into ENGINE.
 const canvas = new Canvas(
-  "gameCanvas",
-  document.getElementById("app"),
-  GAME_WIDTH,
-  GAME_HEIGHT
+    "gameCanvas",
+    document.getElementById("app"),
+    GAME_WIDTH,
+    GAME_HEIGHT
 );
 canvas.create();
 
@@ -21,28 +30,28 @@ const snake = new Snake(canvas.ctx);
 
 //----------------WINDOW EVENT LISTNENERS---------
 window.addEventListener("keydown", function (e) {
-  switch (e.key) {
-    case "ArrowUp":
-      //KEEP TRACK OF DIRECTION OF THE SNAKE.
-      snake.updateCurrentDirection("up");
+    switch (e.key) {
+        case "ArrowUp":
+            //KEEP TRACK OF DIRECTION OF THE SNAKE.
+            snake.currentDirection = "up";
 
-      break;
-    case "ArrowDown":
-      snake.updateCurrentDirection("down");
+            break;
+        case "ArrowDown":
+            snake.currentDirection = "down";
 
-      break;
-    case "ArrowRight":
-      snake.updateCurrentDirection("right");
+            break;
+        case "ArrowRight":
+            snake.currentDirection = "right";
 
-      break;
-    case "ArrowLeft":
-      snake.updateCurrentDirection("left");
+            break;
+        case "ArrowLeft":
+            snake.currentDirection = "left";
 
-      break;
+            break;
 
-    default:
-      break;
-  }
+        default:
+            break;
+    }
 });
 
 //-------------REQUEST ANIMATION FRAME ---------------
@@ -51,47 +60,43 @@ window.mozRequestAnimationFrame;
 window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
 let deltaTime = 0;
 let oldTimeStamp = 0;
-let fps;
+let fps = 40;
 
 //-------------------GAME LOOP --------------------------
 function gameLoop(timeStamp) {
-  //CLEAR CANVAS
-  canvas.ctx.clearRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
-  // Calculate the number of seconds passed since the last frame
-  //we divide by 1000 to get the delta time in seconds between each frame.
-  deltaTime = (timeStamp - oldTimeStamp) / 1000;
-  //this will have the previous time stamp for the next frame.
-  oldTimeStamp = timeStamp;
-  //calculates the frames per second.
-  fps = Math.round(1 / deltaTime);
+    //!Set timeout is forcing the request animation frame to run at 40fps.
+    setTimeout(() => {
+        //CLEAR CANVAS
+        canvas.ctx.clearRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
+        // Calculate the number of seconds passed since the last frame
+        //we divide by 1000 to get the delta time in seconds between each frame.
+        deltaTime = (timeStamp - oldTimeStamp) / 1000;
+        //this will have the previous time stamp for the next frame.
+        oldTimeStamp = timeStamp;
+        //calculates the frames per second.
+        //------------------SNAKE --------------------
+        snake.snakeList.forEach((snakeListItem, index) => {
+            //HEAD OF SNAKE
+            if (index === 0) {
+                return snake.drawSnake(snakeListItem.x, snakeListItem.y, "black");
+            }
+            // TAIL OF SNAKE
+            else if (index === snake.snakeList.length - 1) {
+                return snake.drawSnake(snakeListItem.x, snakeListItem.y, "red");
+            }
+            // SNAKE BODY
+            snake.drawSnake(snakeListItem.x, snakeListItem.y, "green");
+        });
+        snake.updateSnakeMovement();
 
-  //------------------SNAKE --------------------
-  snake.snakeList.forEach((snakeListItem, index) => {
-    //HEAD OF SNAKE
-    if (index === 0) {
-      return snake.drawSnake(snakeListItem.x, snakeListItem.y, "black");
-    }
-    // TAIL OF SNAKE
-    else if (index === snake.snakeList.length - 1) {
-      return snake.drawSnake(snakeListItem.x, snakeListItem.y, "red");
-    }
-    // SNAKE BODY
-    snake.drawSnake(snakeListItem.x, snakeListItem.y, "green");
-  });
-  snake.updateSnakeMovement();
-
-  //call the game loop for each frame.
-  requestAnimationFrame(gameLoop);
+        //call the game loop for each frame.
+        requestAnimationFrame(gameLoop);
+    }, 1000 / fps);
 }
 //----------------START GAME-------------------
 function startGame() {
-  requestAnimationFrame(gameLoop);
+    requestAnimationFrame(gameLoop);
 }
 
-//Start out game.
+//!GAME START.
 startGame();
-// pass the game root to the Canvas.
-
-// setInterval(() => {
-//   snake.snakeList.unshift({ x: snake.snakeList[0].x + 20, y: 200 });
-// }, 2000);
